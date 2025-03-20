31ecbf334ac64e2e8fad91a7919cd0ee57b149ac
Sigurd Riseth <sigurd.riseth@gmail.com>
2025-03-21 00:18:09 +0100

    Added 'home' route for requests to root.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+// main is the entry point for the application.
+// It starts the HTTP server on port 8080 and registers the routes.
 func main() {
 	log.Println("Starting server...")
 
@@ -12,7 +14,26 @@ func main() {
 	router := http.NewServeMux()
 	port := "8080"
 
+	// Register the routes
+	router.HandleFunc("/", home)
+
 	// Log a message indicating the server has started
 	log.Println("Server started on port " + port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
+}
+
+// home handles HTTP requests to the root path.
+// It supports only the GET method and responds with "Hello, World!".
+// For unsupported methods, it responds with a 405 Method Not Allowed status.
+func home(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case http.MethodGet:
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte("Hello, World!"))
+		return
+	default:
+		log.Println("Unsupported request method " + request.Method)
+		http.Error(writer, "Unsupported request method "+request.Method, http.StatusMethodNotAllowed)
+		return
+	}
 }
