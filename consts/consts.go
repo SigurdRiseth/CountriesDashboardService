2185f7de21be299cdfd25ef2856b4f4dfe74bddb
Theodor Sjetnan Utvik <theodorsjetnanutvik@dhcp-10-24-105-201.wlan.ntnu.no>
2025-03-24 12:47:03 +0100

    Refactored consts features struct out of RegristrationBodyRequest for better readability.

--- consts/consts.go
+++ consts/consts.go
@@ -1,18 +1,20 @@
 package consts
 
+type Features struct {
+	Temperature      bool     `json:"temperature"`
+	Precipitation    bool     `json:"precipitation"`
+	Capital          bool     `json:"capital"`
+	Coordinates      bool     `json:"coordinates"`
+	Population       bool     `json:"population"`
+	Area             bool     `json:"area"`
+	TargetCurrencies []string `json:"targetCurrencies"`
+}
+
 type RegistrationRequestBody struct {
-	Country  string `json:"country"`
-	IsoCode  string `json:"isoCode"`
-	Features struct {
-		Temperature      bool     `json:"temperature"`
-		Precipitation    bool     `json:"precipitation"`
-		Capital          bool     `json:"capital"`
-		Coordinates      bool     `json:"coordinates"`
-		Population       bool     `json:"population"`
-		Area             bool     `json:"area"`
-		TargetCurrencies []string `json:"targetCurrencies"`
-	} `json:"features"`
-	TimeChanged string `json:"timeChanged"` // TODO: Surely not ideal since users can now provide this?
+	Country     string   `json:"country"`
+	IsoCode     string   `json:"isoCode"`
+	Features    Features `json:"features"`
+	TimeChanged string   `json:"timeChanged"` // TODO: Surely not ideal since users can now provide this?
 }
 type RegistrationRequestResponse struct {
 	Id         string `json:"id"`
