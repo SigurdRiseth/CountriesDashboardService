e74d99b2d56977b89cfbebe20d187fccdacab09f
Theodor Sjetnan Utvik <theodorsjetnanutvik@Theodors-MacBook-Pro.local>
2025-04-04 12:44:28 +0200

    Generated test function layout from registrations.go to registrations_test.go.

--- handlers/registrations_test.go
+++ handlers/registrations_test.go
@@ -1,1 +1,203 @@
 package handlers
+
+import (
+	"CountriesDashboardService/consts"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+// Test POST request with valid payload → should return 201 Created with document ID and timestamp.
+
+func TestHandleRegistrations(t *testing.T) {
+	type args struct {
+		writer  http.ResponseWriter
+		request *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			HandleRegistrations(tt.args.writer, tt.args.request)
+		})
+	}
+}
+
+func Test_addDashboardConfiguration(t *testing.T) {
+	type args struct {
+		writer  http.ResponseWriter
+		request *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			addDashboardConfiguration(tt.args.writer, tt.args.request)
+		})
+	}
+}
+
+func Test_deleteDashboardConfiguration(t *testing.T) {
+	type args struct {
+		writer  http.ResponseWriter
+		request *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			deleteDashboardConfiguration(tt.args.writer, tt.args.request)
+		})
+	}
+}
+
+func Test_getAllDashboardConfigsFromDB(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    []consts.RegistrationRequestBody
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getAllDashboardConfigsFromDB()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getAllDashboardConfigsFromDB() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getAllDashboardConfigsFromDB() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getDashboardConfigFromDB(t *testing.T) {
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *consts.RegistrationRequestBody
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getDashboardConfigFromDB(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getDashboardConfigFromDB() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getDashboardConfigFromDB() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_handleHeadRequest(t *testing.T) {
+	type args struct {
+		writer  http.ResponseWriter
+		request *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handleHeadRequest(tt.args.writer, tt.args.request)
+		})
+	}
+}
+
+func Test_handlePatchRequest(t *testing.T) {
+	type args struct {
+		writer  http.ResponseWriter
+		request *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handlePatchRequest(tt.args.writer, tt.args.request)
+		})
+	}
+}
+
+func Test_replaceDashboardConfiguration(t *testing.T) {
+	type args struct {
+		writer  http.ResponseWriter
+		request *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			replaceDashboardConfiguration(tt.args.writer, tt.args.request)
+		})
+	}
+}
+
+func Test_sendErrorResponse(t *testing.T) {
+	type args struct {
+		writer     http.ResponseWriter
+		message    string
+		statusCode int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sendErrorResponse(tt.args.writer, tt.args.message, tt.args.statusCode)
+		})
+	}
+}
+
+func Test_viewDashboardConfiguration(t *testing.T) {
+	type args struct {
+		writer  http.ResponseWriter
+		request *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			viewDashboardConfiguration(tt.args.writer, tt.args.request)
+		})
+	}
+}
